19566f3903d6d802c0138836ea3b73743a1432b7
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-12-07 20:49:31 +0000

    calculations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,18 +9,40 @@ import { CryptoCurrencyData } from '../models/crypto-currency.data';
 })
 export class HomeComponent implements OnInit {
   data: Array<CryptoCurrencyData>;
-  amount: number;
-  count: number;
+  amount: 1000;
+  count: 10;
+  currency = 'usd';
+  portfolio: Array<any>;
 
   constructor(private readonly marketDataService: MarketDataService) { }
 
   ngOnInit() {
     this.marketDataService
-      .getMarketData(10, "EUR")
-      .subscribe(data => this.data = data);
+      .getMarketData(10, 'EUR')
+      .subscribe(data => {
+        this.data = data;
+        this.update();
+      });
   }
 
   update() {
+    if (this.amount > 0
+      && this.count > 0) {
+      const currencies = this.data
+        .slice(0, this.count);
+      const currenciesCap = currencies
+        .map(c => this.getMarketCap(c, this.currency))
+        .reduce((accumulator, cap) => Number(accumulator) + Number(cap));
+      this.portfolio = currencies
+        .map(c => {
+          const currencyAmount = this.getMarketCap(c, this.currency) / currenciesCap * this.amount;
+          return currencyAmount;
+        });
+    }
+  }
 
+  getMarketCap(cryptoCurrencyData: CryptoCurrencyData, currency: string): number {
+    const marketCap = cryptoCurrencyData['market_cap_' + currency.toLowerCase()];
+    return marketCap;
   }
 }
